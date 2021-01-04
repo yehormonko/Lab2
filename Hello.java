ceef6fdebfe13087777c8dbf33eff63a76bedd76
Monko Yehor <monkoyehor@gmail.com>
2021-01-04 15:28:11 +0200

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
-public class Hello {
+public class Hello
+{
 public static void main( String argv[] )
 {
 // Default is "World"
+// Author: Jim Weirich (jim@somewhere.com)
 String name = "World";
 if ( argv.length != 0 )
 {
