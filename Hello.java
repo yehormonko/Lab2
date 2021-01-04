7bee7822ef50541ba4340173708c19a8e1246fe1
Monko Yehor <monkoyehor@gmail.com>
2021-01-04 15:08:29 +0200

    Added a default value

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
-public static void main( String argv[] ) {
-System.out.println( "Hello, " + argv[0] + "!" );
+public static void main( String argv[] )
+{
+String name = "World";
+if ( argv.length != 0 )
+{
+name = argv[0];
+}
+System.out.println( "Hello, " + name + "!" );
 }
 }
